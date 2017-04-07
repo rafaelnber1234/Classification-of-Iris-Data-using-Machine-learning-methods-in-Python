0fcfcb5ee072b153e0dbd8ecbc241baa0a92b7ea
girishp92 <girishfrombangalore@gmail.com>
2017-04-07 13:28:51 -0400

    Add files via upload

---
+++ out.py
@@ -0,0 +1,83 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: C:\Python27\IrisTest.py ======================
+LR: 0.966667 (0.040825)
+0.9
+[[ 7  0  0]
+ [ 0 11  1]
+ [ 0  2  9]]
+                 precision    recall  f1-score   support
+
+    Iris-setosa       1.00      1.00      1.00         7
+Iris-versicolor       0.85      0.92      0.88        12
+ Iris-virginica       0.90      0.82      0.86        11
+
+    avg / total       0.90      0.90      0.90        30
+
+LDA: 0.975000 (0.038188)
+0.9
+[[ 7  0  0]
+ [ 0 11  1]
+ [ 0  2  9]]
+                 precision    recall  f1-score   support
+
+    Iris-setosa       1.00      1.00      1.00         7
+Iris-versicolor       0.85      0.92      0.88        12
+ Iris-virginica       0.90      0.82      0.86        11
+
+    avg / total       0.90      0.90      0.90        30
+
+KNN: 0.983333 (0.033333)
+0.9
+[[ 7  0  0]
+ [ 0 11  1]
+ [ 0  2  9]]
+                 precision    recall  f1-score   support
+
+    Iris-setosa       1.00      1.00      1.00         7
+Iris-versicolor       0.85      0.92      0.88        12
+ Iris-virginica       0.90      0.82      0.86        11
+
+    avg / total       0.90      0.90      0.90        30
+
+CART: 0.975000 (0.038188)
+0.9
+[[ 7  0  0]
+ [ 0 11  1]
+ [ 0  2  9]]
+                 precision    recall  f1-score   support
+
+    Iris-setosa       1.00      1.00      1.00         7
+Iris-versicolor       0.85      0.92      0.88        12
+ Iris-virginica       0.90      0.82      0.86        11
+
+    avg / total       0.90      0.90      0.90        30
+
+NB: 0.975000 (0.053359)
+0.9
+[[ 7  0  0]
+ [ 0 11  1]
+ [ 0  2  9]]
+                 precision    recall  f1-score   support
+
+    Iris-setosa       1.00      1.00      1.00         7
+Iris-versicolor       0.85      0.92      0.88        12
+ Iris-virginica       0.90      0.82      0.86        11
+
+    avg / total       0.90      0.90      0.90        30
+
+SVM: 0.991667 (0.025000)
+0.9
+[[ 7  0  0]
+ [ 0 11  1]
+ [ 0  2  9]]
+                 precision    recall  f1-score   support
+
+    Iris-setosa       1.00      1.00      1.00         7
+Iris-versicolor       0.85      0.92      0.88        12
+ Iris-virginica       0.90      0.82      0.86        11
+
+    avg / total       0.90      0.90      0.90        30
+
+>>> 
